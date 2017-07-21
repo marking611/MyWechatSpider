8d53a151f2391e9208ab213945ba1174391b2cc3
marking611 <178431729@qq.com>
2017-07-21 22:27:45 +0800

    Initial commit

---
+++ src/main/java/com/mak/MyWechatSpiderApplication.java
@@ -0,0 +1,12 @@
+package com.mak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWechatSpiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWechatSpiderApplication.class, args);
+	}
+}
